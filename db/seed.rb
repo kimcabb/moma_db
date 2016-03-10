ee6a5efd163d62e1fb14633e2be2e6f9d73d3def
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-03-10 06:08:48 -0500

    modified seed file

--- db/seed.rb
+++ db/seed.rb
@@ -6,3 +6,10 @@ require "pry"
 require_relative "../models/student"
 require_relative "../models/instructor"
 require_relative "../db/connection.rb"
+
+Artist.destroy_all
+Painting.destroy_all
+
+# 'Starry Night' by Vincent van Gogh (Dutch)
+# 'Guernica' by Pablo Picasso (Spanish)
+# 'Water Lilies' by Claude Money (French)
