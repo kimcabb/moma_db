35dfc1d96e09a21a5d8a819099911767fcc1aa88
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-03-14 14:42:13 -0400

    updates

--- ar_commands.rb
+++ ar_commands.rb
@@ -23,15 +23,21 @@ Artist.find(2)
 Artist.find(name:"Vincent van Gogh")
 
 # 7. Return all the artists from Spain
+Artist.where(nationality: "Spanish")
 
 # 8. Return the painting with an id of 1
+Painting.find(1)
 
 # 9. Return the painting with a name of "Guernica"
+Painting.where(name:"Guernica")
 
 # 10. Return all the paintings that "Pablo Picasso" painted
+Painting.where(artist:"Pablo Picasso")
 
 # 11. Return the artist of "Guernica"
+Artist.where(painting:"Guernica")
 
 # 12. Change Vincent Van Gogh's nationality to USA
+vGogh.update(nationality:"USA")
 
 # 13. Change "Starry Night" to be one of Pablo Picasso's paintings
