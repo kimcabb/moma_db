f6ad7deec4bc1b489552ae3112b01182e491d66a
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-03-14 14:32:19 -0400

    updates, nnot sure this branch updated

--- ar_commands.rb
+++ ar_commands.rb
@@ -5,16 +5,22 @@ require_relative "models/artist"
 require_relative "models/painting"
 
 # 1. Create a new Artist (Vincent Van Gogh) and save him to your database
+vGogh = Artist.create(name:"Vincent van Gogh", nationality:"Spanish")
 
 # 2. Create a new Painting (Starry Night) and save it to your database
+vGogh.painting.create(title:"Starry Night")
 
 # 3. Return all Artists
+Artist.all
 
 # 4. Return all Paintings
+Painting.all
 
 # 5. Return the artist with an id of 2
+Artist.find(2)
 
 # 6. Return the artist with a name of "Vincent Van Gogh"
+Artist.find(name:"Vincent van Gogh")
 
 # 7. Return all the artists from Spain
 
--- db/seed.rb
+++ db/seed.rb
@@ -3,13 +3,16 @@ require "pg"
 require "active_record"
 require "pry"
 
-require_relative "../models/student"
-require_relative "../models/instructor"
+require_relative "../models/artist"
+require_relative "../models/painting"
 require_relative "../db/connection.rb"
 
 Artist.destroy_all
 Painting.destroy_all
 
-# 'Starry Night' by Vincent van Gogh (Dutch)
-# 'Guernica' by Pablo Picasso (Spanish)
-# 'Water Lilies' by Claude Money (French)
+vGogh = Artist.create(name:"Vincent van Gogh", nationality: "Dutch")
+vGogh.painting.create(title:"Starry Night")
+dali = Artist.create(name:"Salvador Dali", nationality: "Spanish")
+dali.painting.create(title:"The Soft Watch")
+samba = Artist.create(name:"Cheri Samba", nationality: "Congolese")
+vGogh.painting.create(title:"J'aime la couleur")
